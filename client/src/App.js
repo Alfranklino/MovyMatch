4ef56cb8d7a03d6a13413a3dd581cfe2133af450
jonathandsilva2 <jonathandsilva2@gmail.com>
2019-08-08 16:12:11 -0400

    logout is working fine

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,57 @@
-import React from "react";
-import axios from "axios";
-import "./App.css";
+import React from 'react';
+import axios from 'axios';
+import './App.css';
 // =====================Apollo & Related=================
-import client from "./utils/apolloClient";
-import { ApolloProvider } from "react-apollo";
+import client from './utils/apolloClient';
+import { ApolloProvider } from 'react-apollo';
 // =====================Lodash====================
-import { without } from "lodash";
-import { findIndex } from "lodash";
-import { uniq } from "lodash";
-import { uniqBy } from "lodash";
-import { forEach } from "lodash";
-import { intersection } from "lodash";
+import { without } from 'lodash';
+import { findIndex } from 'lodash';
+import { uniq } from 'lodash';
+import { uniqBy } from 'lodash';
+import { forEach } from 'lodash';
+import { intersection } from 'lodash';
 // =====================Context, Providers====================
-import { MovieProvider } from "./components/context/MovieContext";
-import { ViewerProvider } from "./components/context/ViewerContext";
+import { MovieProvider } from './components/context/MovieContext';
+import { ViewerProvider } from './components/context/ViewerContext';
 // =====================Routing====================
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import createHistory from "history/createBrowserHistory";
-// =====================Components=================
-import Nav from "./components/utils/Navigation";
-import Home from "./components/pages/Home";
-import Login from "./components/pages/Login";
-import Signup from "./components/pages/Signup";
-import Logout from "./components/pages/Logout";
-import Movies from "./components/pages/Movies";
-import MyWatchedMovies from "./components/pages/MyWatchedMovies";
-import MyProfile from "./components/pages/MyProfile";
-import MyMatches from "./components/pages/MyMatches";
-import Footer from "./components/utils/Footer";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
-const history = createHistory();
+// =====================Components=================
+import Nav from './components/utils/Navigation';
+import Home from './components/pages/Home';
+import Login from './components/pages/Login';
+import Signup from './components/pages/Signup';
+import Logout from './components/utils/Logout';
+import Movies from './components/pages/Movies';
+import MyWatchedMovies from './components/pages/MyWatchedMovies';
+import MyProfile from './components/pages/MyProfile';
+import MyMatches from './components/pages/MyMatches';
+import Footer from './components/utils/Footer';
+
+const history = createBrowserHistory();
 
 function App(props) {
   return (
     <ApolloProvider client={client}>
       <Router history={history}>
-        <div className='App'>
+        <div className="App">
           <ViewerProvider>
             <Nav />
             <Switch>
               <MovieProvider>
-                <Route path='/' exact component={Home} />
-                <Route path='/login' exact component={Login} />
-                <Route path='/logout' exact component={Logout} />
-                <Route path='/signup' exact component={Signup} />
-                <Route path='/movies' exact component={Movies} />
-                <Route path='/watchedmovies' exact component={MyWatchedMovies} />
-                <Route path='/myprofile' exact component={MyProfile} />
-                <Route path='/mymatches' exact component={MyMatches} />
+                <Route path="/" exact component={Home} />
+                <Route path="/login" exact component={Login} />
+                <Route path="/signup" exact component={Signup} />
+                <Route path="/movies" exact component={Movies} />
+                <Route
+                  path="/watchedmovies"
+                  exact
+                  component={MyWatchedMovies}
+                />
+                <Route path="/myprofile" exact component={MyProfile} />
+                <Route path="/mymatches" exact component={MyMatches} />
               </MovieProvider>
             </Switch>
             <Footer />
--- client/src/components/utils/Navigation.js
+++ client/src/components/utils/Navigation.js
@@ -1,53 +1,51 @@
-import React, { useContext, useEffect } from "react";
-import "../../App.css";
-import { Link } from "react-router-dom";
-import { ViewerContext } from "../context/ViewerContext";
+import React, { useContext, useEffect } from 'react';
+import '../../App.css';
+import { Link } from 'react-router-dom';
+import { ViewerContext } from '../context/ViewerContext';
+import Logout from './Logout';
 
 function Nav() {
   const [viewer, setViewer] = useContext(ViewerContext);
 
   return (
     <nav>
-      <Link className='link' to='/'>
+      <Link className="link" to="/">
         <h3>Logo</h3>
       </Link>
 
-      <ul className='nav-links'>
+      <ul className="nav-links">
         {!viewer && (
-          <Link className='link' to='/login'>
+          <Link className="link" to="/login">
             <li>Login</li>
           </Link>
         )}
-        {viewer && (
-          <Link className='link' to='/logout'>
-            <li>Logout</li>
-          </Link>
-        )}
+
         {!viewer && (
-          <Link className='link' to='/signup'>
+          <Link className="link" to="/signup">
             <li>Signup</li>
           </Link>
         )}
-        <Link className='link' to='/movies'>
+        <Link className="link" to="/movies">
           <li>Movies</li>
         </Link>
         {viewer && (
-          <Link className='link' to='/watchedmovies'>
+          <Link className="link" to="/watchedmovies">
             <li>My Watched Movies</li>
           </Link>
         )}
         {viewer && (
-          <Link className='link' to='/myprofile'>
+          <Link className="link" to="/myprofile">
             <li>My Profile</li>
           </Link>
         )}
         {viewer && (
-          <Link className='link' to='/mymatches'>
+          <Link className="link" to="/mymatches">
             <li>My Matches</li>
           </Link>
         )}
       </ul>
-      {viewer && <p className='hello'>{`Hello, ${viewer}`}</p>}
+      {viewer && <p className="hello">{`Hello, ${viewer}`}</p>}
+      {viewer && <Logout />}
     </nav>
   );
 }
